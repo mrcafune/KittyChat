886a17f85ac6485701d123612633056dff9a5458
B Cafune <101951803+mrcafune@users.noreply.github.com>
2023-06-27 10:32:05 -0700

    Update KittyChat.py
    added first time user setup
    added various commands
    check update notes

--- KittyChat.py
+++ KittyChat.py
@@ -4,10 +4,39 @@ import json
 import time
 import sys
 import re
+import os
 
 def print_multiline(text):
     for line in text.split('\n'):
         print(line)
+
+def clear_screen():
+    if os.name == 'posix':  # for UNIX/Linux/MacOS
+        _ = os.system('clear')
+    else:  # for Windows
+        _ = os.system('cls')
+
+def choose_model():
+    print(Fore.GREEN + "Please select your Model | Available Models:")
+    print(Fore.WHITE + " ")
+    print("(1) gpt-3.5-turbo: Most capable GPT-3.5 model optimized for chat at a reduced cost.\n")
+    print("(2) gpt-3.5-turbo-16k: Same capabilities as gpt-3.5-turbo but with four times the context.\n")
+    print("(3) GPT-4: More capable than any GPT-3.5 model, able to do more complex tasks, and optimized for chat.\n")
+    
+    user_choice = input(Fore.YELLOW + "Enter the corresponding number for the model (without parentheses): ")
+
+    if user_choice == "1":
+        print("Model: gpt-3.5-turbo")
+        return 'gpt-3.5-turbo'
+    elif user_choice == "2":
+        print("Model: gpt-3.5-turbo-16k")
+        return 'gpt-3.5-turbo-16k'
+    elif user_choice == "3":
+        print("Model: gpt-4")
+        return 'gpt-4'
+    else:
+        print("Invalid choice. Defaulting to 'gpt-3.5-turbo'.")
+        return 'gpt-3.5-turbo'
 
 def load_json(filename):
     try:
@@ -31,7 +60,6 @@ def chat_with_gpt(api_key, conversation, model_name):
     return response['choices'][0]['message']['content']
 
 def color_comments(text, comment_color='\033[91m', default_color='\033[0m'):
-    # Replace comments with colored versions
     def color_comment(match):
         return comment_color + match.group(0) + default_color
     text = re.sub(r'#.*', color_comment, text)
@@ -46,14 +74,100 @@ def slow_print_multiline(text, delay=0.01):
             time.sleep(delay)
         sys.stdout.write('\n')
 
-def main():
-    # define model options
-    models = ["text-davinci-002","text-curie-003","gpt-3.5-turbo"]
+def load_and_print_session(filename, session_name):
+    if not os.path.isfile(filename):
+        print("No saved session found.")
+        return None
+    else:
+        with open(filename, 'r') as f:
+            sessions = json.load(f)
+        if session_name in sessions:
+            session = sessions[session_name]
+            print(f"\nSession name: {session_name}")
+            for message in session:
+                if message['role'] == 'system':
+                    print(f"System: {message['content']}")
+                elif message['role'] == 'user':
+                    print(f"\033[94mYou: {message['content']}\033[0m")
+                else:  # message['role'] == 'assistant'
+                    print(f"\033[92mKitten Technologies Virtual Assistant: {message['content']}\033[0m")
+        else:
+            print("No session found with this name.")
 
-# default model
-    current_model = models[2]
+def load_config(filename):
+    if not os.path.isfile(filename):
+        config = {"first_time": True, "api_key": "", "model": ""}
+        with open(filename, 'w') as f:
+            json.dump(config, f)
+        print("No previous configuration file found - configuration file generated.")
+        return config
+    with open(filename, 'r') as f:
+        try:
+            config = json.load(f)
+            # Just making sure that the model key is present
+            if 'model' not in config:
+                config['model'] = ""
+            return config
+        except json.JSONDecodeError:
+            print("Failed to decode the configuration file.")
+            return None
+
+def save_config(filename, config):
+    with open(filename, 'w') as f:
+        json.dump(config, f)
+
+def main():
+    config_filename = 'config.json'
+    config = load_config(config_filename)
+    if config is None:
+        print("Unable to access or parse the config file.")
+        print("Please check your permissions to the file.  Exiting program")
+        exit()
+    elif "first_time" in config and not config["first_time"]:
+        api_key = config["api_key"]
+        model = config["model"]
+        print("Config file loaded.")
+    else:
+        print(Fore.GREEN, " ")
+        print('''
+██╗  ██╗██╗████████╗████████╗██╗   ██╗ ██████╗██╗  ██╗ █████╗ ████████╗
+██║ ██╔╝██║╚══██╔══╝╚══██╔══╝╚██╗ ██╔╝██╔════╝██║  ██║██╔══██╗╚══██╔══╝
+█████╔╝ ██║   ██║      ██║    ╚████╔╝ ██║     ███████║███████║   ██║   
+██╔═██╗ ██║   ██║      ██║     ╚██╔╝  ██║     ██╔══██║██╔══██║   ██║   
+██║  ██╗██║   ██║      ██║      ██║   ╚██████╗██║  ██║██║  ██║   ██║   
+╚═╝  ╚═╝╚═╝   ╚═╝      ╚═╝      ╚═╝    ╚═════╝╚═╝  ╚═╝╚═╝  ╚═╝   ╚═╝   
+                                                                     ''')
+        print(Fore.WHITE, "🐈 KittyChat - Terminal AI Assistant | Written by Brandon K.")
+        print(Fore.YELLOW + "*** Please note that KittyChat does not store any of your personal information ***")
+        print(Fore.WHITE + "Visit https://kittentechnologies.com or https://github.com/mrcafune/kittychat for more information.")
+        print("-------------------------------------------------------------------\n")
+        print(Fore.GREEN + "Welcome to the first time setup!  In order to use this program you will:\n")
+        print(Fore.WHITE + "- Enter your API Key (You can find your Secret API key in your User settings.)\n- Choose your Chat model\n")
+        api_key = input(Fore.YELLOW + "Please enter your OpenAI API without quotes: ")
+        print(Fore.WHITE + "\n-------------------------------------------------------------------\n")
+        model = choose_model()
+        print("\nTesting API Key - this will take a few seconds")
+        try:
+            test_message = chat_with_gpt(api_key, [{"role": "system", "content": "Please reply with just the word, success"}], model)
+            print(test_message)
+        except Exception as e:
+            print("An error occurred: ", e)
+            print(Fore.RED, "API key seems to be incorrect. Please re-enter the API key or type " + Fore.YELLOW, "-exit" + Fore.WHITE, "to exit.")
+            user_input = input()
+            if user_input == '-exit':
+                return
+        config["first_time"] = False
+        config["api_key"] = api_key
+        config["model"] = model
+        save_config(config_filename, config)
+        print(Fore.GREEN, "\nSuccess!")
+        print("Exiting Setup & KittyChat - you can revisit this setup by typing" + Fore.YELLOW + " -reset")
+        time.sleep(10)
+        print(Fore.WHITE, "")
+        clear_screen()
+
+    current_model = model
     print(Fore.GREEN, " ")
-    api_key = ("sk-YOUR-KEY-HERE")
     print('''
 ██╗  ██╗██╗████████╗████████╗██╗   ██╗ ██████╗██╗  ██╗ █████╗ ████████╗
 ██║ ██╔╝██║╚══██╔══╝╚══██╔══╝╚██╗ ██╔╝██╔════╝██║  ██║██╔══██╗╚══██╔══╝
@@ -62,7 +176,7 @@ def main():
 ██║  ██╗██║   ██║      ██║      ██║   ╚██████╗██║  ██║██║  ██║   ██║   
 ╚═╝  ╚═╝╚═╝   ╚═╝      ╚═╝      ╚═╝    ╚═════╝╚═╝  ╚═╝╚═╝  ╚═╝   ╚═╝   
                                                                      ''')
-    print(Fore.WHITE, "Version 0.2 | Written by Brandon K.\n" + Fore.GREEN, "Powered by Love ❤️ & Python 🐍")
+    print(Fore.WHITE, "Version 1.0 | Written by Brandon K.\n" + Fore.GREEN, "Powered by Love ❤️ & Python 🐍")
     print(Fore.WHITE, " ")
     print("-------------------------------------------------------------------")
     print("Please type" + Fore.YELLOW, "-help" + Fore.WHITE, "for switches and additional options")
@@ -109,6 +223,9 @@ def main():
             print("Configuration Changed: -neverask" + Fore.RED," [OFF]")
             save_json(config_filename, config)
             continue
+        elif user_message.lower() == '-clearscreen':
+            clear_screen()
+            continue
         elif user_message.lower() == '-neverask':
             config['alwaysask'] = False
             config['neverask'] = True
@@ -116,27 +233,30 @@ def main():
             print("Configuration Changed: -alwaysask" + Fore.RED," [OFF]")
             save_json(config_filename, config)
             continue
+        elif user_message.lower() == '-reset':
+            config["first_time"] = True
+            save_config(config_filename, config)
+            print("Reset complete. You'll be asked for API key the next time you open this program\n KittyChat will now close.")
+            exit()
         elif user_message.lower() == '-help':
             print(Fore.YELLOW + "Available Commands")
-            print(Fore.YELLOW + "\n-exit:" + Fore.WHITE +"          Exit the conversation. You'll be prompted to save the conversation.")
-            print(Fore.YELLOW + "-alwaysask:" + Fore.WHITE + "     Always ask at startup whether to start a new conversation or load a previous one.")
-            print(Fore.YELLOW + "-neverask" + Fore.WHITE + "       Never ask at startup, always start a new conversation.")
-            print(Fore.YELLOW + "-help:" + Fore.WHITE + "          Display this help message.")
-            print(Fore.YELLOW + "-model: " + Fore.RED + "        [BETA]" + Fore.WHITE, "Changes the language learning model of the app.")
+            print(Fore.YELLOW + "\n-exit:" + Fore.WHITE +"             Exit the conversation. You'll be prompted to save the conversation")
+            print(Fore.YELLOW + "-alwaysask:" + Fore.WHITE + "        Always ask at startup whether to start a new conversation or load a previous one")
+            print(Fore.YELLOW + "-neverask" + Fore.WHITE + "          Never ask at startup, always start a new conversation")
+            print(Fore.YELLOW + "-clearscreen" + Fore.WHITE + "       Clears the terminal of text")
+            print(Fore.YELLOW + "-help:" + Fore.WHITE + "             Display this help message")
+            print(Fore.YELLOW + "-model:" + Fore.WHITE + "            Changes the language learning model")
+            print(Fore.YELLOW + "-sessionsummary:" + Fore.WHITE + "   Prints the previous sessions conversation")
+            print(Fore.YELLOW + "-reset:" + Fore.WHITE + "            Clears your API Key, runs first time setup")
             continue
         elif user_message.lower() == '-model':
-            print("Current Model: " + current_model)
-            print(" ")
-            print("Available models:\n")
-            print("1 - GPT 3.5 Turbo")
-            print("2 - Davinci-002")
-            print("3 - Currie-003")
-            print(" ")
-            model_number_st = input("[BETA][NOT ALWAYS FUNCTIONAL] Please enter your desired Model by entering just the corresponding number: ")
-            model_number = int(model_number_st)
-            model_number -= 1
-            current_model = models[model_number -1]
-            print("Model changed to: " + current_model)
+            choose_model()
+            continue
+        elif user_message.lower() == '-sessionsummary':
+            if session_name is not None:
+                load_and_print_session(session_filename, session_name)
+            else:
+                print("No session has been started yet.")
             continue
         elif user_message.lower()[0] == '-':
             print("Unrecognized command. Available commands are -exit, -alwaysask, -neverask, -help.")
@@ -146,7 +266,7 @@ def main():
         conversation.append({"role": "user", "content": user_message})
         
         bot_message = chat_with_gpt(api_key, conversation, current_model)
-        print('\033[92m' + Fore.GREEN, 'Kitten Technologies Virtual Assistant: ' + '\033[0m')
+        print('\033[92m' + Fore.GREEN + 'Kitten Technologies Virtual Assistant: ' + '\033[0m')
         print(Fore.WHITE, end=' ')
         slow_print_multiline(bot_message)
         conversation.append({"role": "assistant", "content": bot_message})
